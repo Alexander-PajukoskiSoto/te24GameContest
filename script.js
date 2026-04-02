8d8a88ea5f0927b877b9237583f4f11a5a65bf17
Alexander Pajukoski Soto <145021057+Alexander-PajukoskiSoto@users.noreply.github.com>
2026-04-02 09:40:59 +0200

    Added random array

--- script.js
+++ script.js
@@ -16,21 +16,26 @@ const gameList = [
     }
 ];
 
-const main = document.getElementById("games");
-
-for (let i = 0; i < gameList.length; i++) {
-    const game = gameList[i];
+const randomPush = ()=>{
+    const main = document.getElementById("games");
+    const gameLength = gameList.length;
+    for (let i = 0; i < gameLength; i++) {
+        // Makes random number between list length and 0
+        gameIndex = Math.floor(Math.random() *gameList.length)
+        const game = gameList[gameIndex];
+        // 1. Create the section
+        let gameSection = document.createElement("section");
+        gameSection.className = "gameCard";
     
-    // 1. Create the section
-    let gameSection = document.createElement("section");
-    gameSection.className = "gameCard";
-
-    // 2. Create the image
-    let gameImage = document.createElement("img");
-    gameImage.src = game.image;
-    gameImage.alt = game.name;
-
-    // 3. Put it all together
-    gameSection.append(gameImage);
-    main.append(gameSection);
+        // 2. Create the image
+        let gameImage = document.createElement("img");
+        gameImage.src = game.image;
+        gameImage.alt = game.name;
+    
+        // 3. Put it all together
+        gameSection.append(gameImage);
+        main.append(gameSection);
+        gameList.splice(gameIndex,1);
+    }
 }
+randomPush();
