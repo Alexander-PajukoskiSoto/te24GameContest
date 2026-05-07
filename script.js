5811cff45b35d484dbe257e37ace98d1b729ecb4
Alexander Pajukoski Soto <145021057+Alexander-PajukoskiSoto@users.noreply.github.com>
2026-05-07 09:38:20 +0200

    Dont know what wrong
    Need to figure out the random loop. Just gives 2 of 3 elements

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
-const gameList = [
-    {
-        name: "Smurf Crashout",
-        url: "#",
-        image: "./images/Robloxhead.webp"
-    },
-    {
-        name: "Some other game",
-        url: "#",
-        image: "./images/Robloxhead.webp"
-    },
-    {
-        name: "Johannes Douglas",
-        url: "#",
-        image: "./images/Robloxhead.webp"
-    }
-];
+
 
 const randomPush = ()=>{
-    const main = document.getElementById("games");
+    const gameList = [
+        {
+            name: "Smurf Crashout",
+            url: "#",
+            image: "./images/Robloxhead.webp"
+        },
+        {
+            name: "Some other game",
+            url: "#",
+            image: "./images/Robloxhead.webp"
+        },
+        {
+            name: "Johannes Douglas",
+            url: "#",
+            image: "./images/Robloxhead.webp"
+        }
+    ];
+    let randomGameList = [];
     const gameLength = gameList.length;
+    const main = document.getElementById("games");
     for (let i = 0; i < gameLength; i++) {
-        // Makes random number between list length and 0
-        gameIndex = Math.floor(Math.random() *gameList.length)
-        const game = gameList[gameIndex];
-        // 1. Create the section
+        let gameIndex = Math.floor(Math.random() * gameList.length);
+        randomGameList[i] = gameList[gameIndex];
+        gameList.splice(gameIndex,1);
+    }
+    //  Makes random number between list length and 0
+    //  Create the section
+    console.log(randomGameList);
+
+    console.log(randomGameList);
+    for (let i = 0; i < randomGameList.length; i++) {
+        const game = randomGameList[i];
         let gameSection = document.createElement("section");
         gameSection.className = "gameCard";
-    
-        // 2. Create the image
+        gameSection.id = randomGameList[i + 1].name;
+
+        //  Create the image
         let gameImage = document.createElement("img");
         gameImage.src = game.image;
         gameImage.alt = game.name;
-    
-        // 3. Put it all together
+
+        //  Create next game link
+        let nextLink = document.createElement("a");
+        let nextText = document.createTextNode(">");
+        nextLink.setAttribute("href",`#${randomGameList[i].name}`)
+        nextLink.append(nextText);
+        
+        //  Put it all together
         gameSection.append(gameImage);
         main.append(gameSection);
-        gameList.splice(gameIndex,1);
-    }
+        gameSection.append(nextLink);
+        randomGameList.splice(i,1);
+        }
+    
 }
 randomPush();
